ab095e95a60937fe5b88b32c558e67c5001186c1
Pablo98767 <89041026+Pablo98767@users.noreply.github.com>
2022-07-05 20:53:36 -0300

    Site Banda Mega Alfa
    Veja o script fonte da banda Mega Alfa (Infelizmente atualmente descontinuada). Mas seu site e código fonte continua por aqui para que vocês podem ver.

---
+++ scripts.js
@@ -0,0 +1,14 @@
+
+function hover(){
+  
+    document.getElementById("contact").style.color = "red";
+
+}
+
+function houverOut(){
+    document.getElementById("contact").style.color ="white";
+
+    
+}
+
+
